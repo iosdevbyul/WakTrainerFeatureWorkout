0f63342b5515a599c414a1ed0966244b40ba3e72
Tris <hbabnd@gmail.com>
2026-09-19 17:58:58 +0900

    refactor - clean up workout feature package boundaries

--- Package.swift
+++ Package.swift
@@ -11,18 +11,58 @@ let package = Package(
         .library(name: "WakTrainerFeatureWorkout", targets: ["WakTrainerFeatureWorkout"])
     ],
     dependencies: [
-        .package(url: "https://github.com/iosdevbyul/WakTrainerCoreModels", branch: "main"),
-        .package(url: "https://github.com/iosdevbyul/WakTrainerDomainWorkout", branch: "main"),
-        .package(url: "https://github.com/iosdevbyul/WakTrainerFeatureTimer", branch: "main")
+        .package(
+            url: "https://github.com/iosdevbyul/WakTrainerCoreModels",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/iosdevbyul/WakTrainerDomainWorkout",
+            branch: "refactor/domain-boundaries"
+        ),
+        .package(
+            url: "https://github.com/iosdevbyul/WakTrainerFeatureTimer",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/iosdevbyul/WakTrainerServiceLocation",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/iosdevbyul/WakTrainerServiceHealthKit",
+            branch: "main"
+        )
     ],
     targets: [
         .target(
             name: "WakTrainerFeatureWorkout",
             dependencies: [
-                .product(name: "WakTrainerCoreModels", package: "WakTrainerCoreModels"),
-                .product(name: "WakTrainerDomainWorkout", package: "WakTrainerDomainWorkout"),
-                .product(name: "WakTrainerFeatureTimer", package: "WakTrainerFeatureTimer")
+                .product(
+                    name: "WakTrainerCoreModels",
+                    package: "WakTrainerCoreModels"
+                ),
+                .product(
+                    name: "WakTrainerDomainWorkout",
+                    package: "WakTrainerDomainWorkout"
+                ),
+                .product(
+                    name: "WakTrainerFeatureTimer",
+                    package: "WakTrainerFeatureTimer"
+                ),
+                .product(
+                    name: "WakTrainerServiceLocation",
+                    package: "WakTrainerServiceLocation"
+                ),
+                .product(
+                    name: "WakTrainerServiceHealthKit",
+                    package: "WakTrainerServiceHealthKit"
+                )
             ]
-        )
-    ]
+        ),
+        .testTarget(
+                name: "WakTrainerFeatureWorkoutTests",
+                dependencies: [
+                    "WakTrainerFeatureWorkout"
+                ]
+            )
+    ],
 )
