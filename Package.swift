4e6768a488b52293711196d3eaddfb822c88e862
Tris <hbabnd@gmail.com>
2026-09-19 18:28:16 +0900

    add - implement workout selection flow

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "WakTrainerFeatureWorkout",
     platforms: [
-        .iOS(.v14),
+        .iOS(.v17),
         .macOS(.v13)
     ],
     products: [
